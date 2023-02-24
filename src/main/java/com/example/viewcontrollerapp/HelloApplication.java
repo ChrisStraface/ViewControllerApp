fffb9b70c070418df5eaf9f15177ab77544392bd
ChrisStraface <chstraface@gmail.com>
2023-02-24 12:54:16 -0500

    Added autosave and autorestore

--- src/main/java/com/example/viewcontrollerapp/HelloApplication.java
+++ src/main/java/com/example/viewcontrollerapp/HelloApplication.java
@@ -8,15 +8,19 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    HelloController viewController;
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 470, 441);
+        viewController = fxmlLoader.getController();
         stage.setTitle("List Viewer");
         stage.setScene(scene);
         stage.show();
     }
-
+    public void stop() throws Exception {
+        viewController.saveData();
+    }
     public static void main(String[] args) {
         launch();
     }
--- src/main/java/com/example/viewcontrollerapp/HelloController.java
+++ src/main/java/com/example/viewcontrollerapp/HelloController.java
@@ -1,9 +1,14 @@
 package com.example.viewcontrollerapp;
 
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.TextFieldListCell;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,12 +26,12 @@ public class HelloController {
     public List<String> list = new ArrayList<String>();
     public List<String> list2 = new ArrayList<String>();
 
-
-    public void initialize() {
-
+    public void initialize() throws Exception {
+    restoreData();
     }
     public void enter() {
         String newTask =  textField.getText();
+        ArrayList<String> temporaryList = new ArrayList<>(listView.getItems());
         list.add(newTask);
         listView.setEditable(true);
         listView.setCellFactory(TextFieldListCell.forListView());
@@ -37,6 +42,7 @@ public class HelloController {
             list.remove(newTask);
             listView.getItems().remove(newTask);
             deleteMenu.getItems().remove(menuItem);
+            temporaryList.remove();
         });
 
     }
@@ -54,6 +60,38 @@ public class HelloController {
             deleteMenu2.getItems().remove(menuItem2);
 
         });
+    }
+
+    public void saveData() throws Exception {
+        FileOutputStream fileOut =
+                new FileOutputStream("SavedTaskList");
+        ObjectOutputStream out = new ObjectOutputStream(fileOut);
+        ArrayList<String> temporaryList = new ArrayList<>(listView.getItems());
+        out.writeObject(temporaryList);
+        ArrayList<String> temporaryList2 = new ArrayList<>(listView2.getItems());
+        out.writeObject(temporaryList2);
+        out.close();
+        fileOut.close();
+    }
+    public void restoreData() throws Exception {
+        FileInputStream fileIn = new FileInputStream("SavedTaskList");
+        ObjectInputStream in = new ObjectInputStream(fileIn);
+        list = (ArrayList<String>) in.readObject();
+        ObservableList tempObservableList = FXCollections.observableArrayList(list);
+        listView.setItems(tempObservableList);
+        list2 = (ArrayList<String>) in.readObject();
+        ObservableList tempObservableList2 = FXCollections.observableArrayList(list2);
+        listView2.setItems(tempObservableList2);
+        for (Object newTask : tempObservableList) {
+            MenuItem menuItem = new MenuItem((String)newTask);
+            deleteMenu.getItems().add(menuItem);
+        }
+        for (Object newTask2 : tempObservableList2) {
+            MenuItem menuItem2 = new MenuItem((String)newTask2);
+            deleteMenu2.getItems().add(menuItem2);
+        }
+        in.close();
+        fileIn.close();
     }
 
 }
